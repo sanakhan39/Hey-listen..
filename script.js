8c8105e1a74a3f84c5badd3681522f0b191816c6
sanakhan39 <ssanakhan786191141@gmail.com>
2024-09-16 17:37:22 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 function checkPassword() {
     const passwordInput = document.getElementById('passwordInput').value;
-    const correctPassword = '1234'; // Set your desired password here
+    const correctPassword = 'katto'; // Set your desired password here
 
     if (passwordInput === correctPassword) {
         document.getElementById('passwordPrompt').style.display = 'none';
